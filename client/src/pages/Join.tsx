8f3acf207db19e2de88025b9f73497d4eba255fd
somn45 <somnsomn45@gmail.com>
2022-06-27 22:04:56 +0900

    Feat : 회원가입 시스템 추가
    이메일, 비밀번호 인증 방식의 회원가입 추가
    회원가입이 완료된 후 회원 정보는 firestore로 넘겨짐

--- client/src/pages/Join.tsx
+++ client/src/pages/Join.tsx
@@ -1,13 +1,22 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+
+const SERVER_ENDPOINT = 'http://localhost:3001';
 
 function Join() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const onClick = (e: React.MouseEvent<HTMLInputElement>) => {
+  const onClick = async (e: React.MouseEvent<HTMLInputElement>) => {
     e.preventDefault();
     if (!(email && password && password.length >= 6)) return;
-    console.log('join');
+    const emailReg = new RegExp(/\w+@\w+.\w+/);
+    if (!emailReg.exec(email)) return;
+    const response = await axios.post(`${SERVER_ENDPOINT}/join`, {
+      email,
+      password,
+    });
+    console.log(response);
   };
   return (
     <div>
--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,34 @@
 import express from 'express';
+import cors from 'cors';
+import { auth } from './firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
-console.log('Hello World!!');
+interface JoinBody {
+  email: string;
+  password: string;
+}
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post('/join', async (req: express.Request, res: express.Response) => {
+  const { email, password }: JoinBody = req.body;
+  try {
+    const response = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    return res.json({
+      uid: response.user.uid,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(PORT, () => console.log(`Server connected in port ${PORT}`));
