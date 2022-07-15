2ea0ff76f9bbcfb00c6feddf42a16ad501ad22ab
somn45 <somnsomn45@gmail.com>
2022-07-15 22:03:11 +0900

    Fix : Redux-toolkit 사용 중 경고 메시지 발생 현상 해결(#1)
    경고 메시지의 원인은 컴포넌트 랜더링 중 함수 호출
    mapDispatchToProps 함수 사용 대신 useDispatch를 사용하는 것으로 해결
    
    해결 : close #1

--- client/src/components/Genre.tsx
+++ client/src/components/Genre.tsx
@@ -1,11 +1,29 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addGenre, removeGenre } from '../store/store';
+
 interface GenreProps {
   genre: string;
 }
 
 function Genre({ genre }: GenreProps) {
+  const dispatch = useDispatch();
+  const [checked, setChecked] = useState(false);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked((prevState) => !prevState);
+    const currentElement = e.target;
+    currentElement.checked
+      ? dispatch(addGenre(currentElement.name))
+      : dispatch(removeGenre(currentElement.name));
+  };
   return (
     <>
-      <input type="checkbox" name={genre} value={genre} />
+      <input
+        type="checkbox"
+        name={genre}
+        checked={checked}
+        onChange={handleChange}
+      />
       {genre}
     </>
   );
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,37 +2,67 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Cookies } from 'react-cookie';
 import axios from 'axios';
+import { connect } from 'react-redux';
 import Genre from '../components/Genre';
+
+interface HomeProps {
+  selectedGenre: string[];
+}
 
 const cookies = new Cookies();
 
-function Home() {
+function Home({ selectedGenre }: HomeProps) {
   const navigate = useNavigate();
-  const [genres, setGenres] = useState([]);
+  const [genres, setGenres] = useState<string[]>([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     getSpotifyGenres();
     if (!cookies.get('F_UID')) navigate('/login');
   }, []);
-  console.log(genres);
   const getSpotifyGenres = async () => {
     const accessToken = cookies.get('accessToken');
     const response = await axios.post(`http://localhost:3001/genres`, {
       accessToken: accessToken,
     });
     setGenres(response.data.genres);
+    setLoading(false);
+  };
+
+  const onClick = async (e: React.MouseEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    /*
+    const accessToken = cookies.get('accessToken');
+    console.log(accessToken);
+    const response = await axios.get(
+      `http://localhost:3001/search?accessToken=${accessToken}`
+    );
+    */
   };
   return (
     <div>
       <h2>Home</h2>
       <form>
-        <div>
-          {genres.map((genre) => (
-            <Genre key={genre} genre={genre} />
-          ))}
-        </div>
+        {loading ? (
+          <h2>장르 목록을 불러오는 중입니다.</h2>
+        ) : (
+          <>
+            {genres.map((genre) => (
+              <Genre key={genre} genre={genre} />
+            ))}
+          </>
+        )}
+        <input
+          type="submit"
+          value="Search your favorite genres"
+          onClick={onClick}
+        />
       </form>
     </div>
   );
 }
 
-export default Home;
+function mapStateToProps(state: string[]) {
+  return { selectedGenre: state };
+}
+
+export default connect(mapStateToProps)(Home);
