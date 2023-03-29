09b437b903859d74afa9209835674bb6d8f276eb
somn45 <somnsomn45@gmail.com>
2023-03-29 23:30:33 +0900

    fix : 서버 경로 local을 aws 서버 주소로 변경

--- client/src/constants/constants.ts
+++ client/src/constants/constants.ts
@@ -1,4 +1,4 @@
-export const SERVER_ENDPOINT = 'http://localhost:3001';
+export const SERVER_ENDPOINT = 'https://mucketlist-server.site';
 export const SPOTIFY_AUTH_URL_CONFIG = {
   response_type: 'code',
   client_id: process.env.REACT_APP_SPOTIFY_CLIENT_ID
